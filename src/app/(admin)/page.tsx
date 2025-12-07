d08ff7b7344619e14dc31ed1402648e5ad6a3b69
unknown <adamridwanfirm@gmail.com>
2025-12-07 20:36:03 +0700

    new files and folders

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -5,12 +5,6 @@ import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
 import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
 import RecentOrders from "@/components/ecommerce/RecentOrders";
 
-export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
-};
-
 export default function Ecommerce() {
   return (
     <div className="grid grid-cols-12 gap-4 md:gap-6">
